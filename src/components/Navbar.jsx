1f8cb5526b6cbab1da12ae56ef3e52224eefb774
Anuj-Gupta79 <anujgupta7906@gmail.com>
2022-12-18 02:06:52 +0530

    Done For the day

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const NavButton = ({title, customFunc, icon, color, dotColor}) =>(
 )
 
 const Navbar = () => {
-  const {activeMenu, setActiveMenu} = useStateContext();
+  const {activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick} = useStateContext();
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
@@ -35,36 +35,41 @@ const Navbar = () => {
       <div className="flex">
         <NavButton
           title="Cart"
-          // customFunc={() => handleClick('Cart')}
+          customFunc={() => handleClick('cart')}
           color="blue"
           icon={<FiShoppingCart/>}
         />
         <NavButton
           title="Chat"
-          // customFunc={() => handleClick('Chat')}
+          customFunc={() => handleClick('chat')}
           color="blue"
           icon={<BsChatLeft/>}
           dotColor="#03C9D7"
         />
         <NavButton
           title="Notifications"
-          // customFunc={() => handleClick('notification')}
+          customFunc={() => handleClick('notification')}
           color="blue"
           icon={<RiNotification3Line/>}
           dotColor="#03C9D7"
         />
 
         <TooltipComponent content="Profile" position="BottomCenter">
-        {/* handleClick('userProfile') */}
-          <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={()=> {}}>
+        
+          <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={()=> handleClick('userProfile')}>
             <img className="rounded-full w-8 h-8" src={avatar} alt="Internet_Error" />
             <p>
               <span className="text-gray-400 text-14">Hi, </span>{' '}
               <span className="text-gray-400 font-bold ml-1 text-14">Anuj</span>
             </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14"/>
           </div>
-
         </TooltipComponent>
+
+        {isClicked.cart && <Cart/>}
+        {isClicked.chat && <Chat/>}
+        {isClicked.notification && <Notification/>}
+        {isClicked.userProfile && <UserProfile/>}
       </div>
     </div>
   )
